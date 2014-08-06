00a04a1155c050d425508ee68c4d74c97192f35f
Igings <igings@hotmail.co.uk>
2014-08-06 21:02:09 +0100

    Updated creative tab localisation

--- src/main/java/com/igings/letsmodreboot/creativetab/CreativeTabLMRB.java
+++ src/main/java/com/igings/letsmodreboot/creativetab/CreativeTabLMRB.java
@@ -9,18 +9,12 @@ import net.minecraft.item.Item;
 //Creative tab can be set in either the item/block declaration or the general block handler
 public class CreativeTabLMRB
 {
-    public static final CreativeTabs LMRB_TAB = new CreativeTabs(Reference.MOD_ID)
+    public static final CreativeTabs LMRB_TAB = new CreativeTabs(Reference.MOD_ID.toLowerCase())
     {
         @Override
         public Item getTabIconItem()
         {
             return ModItems.mapleLeaf;
-        }
-
-        @Override
-        public String getTranslatedTabLabel()
-        {
-            return "Let's Mod Reboot";
         }
 
     };
