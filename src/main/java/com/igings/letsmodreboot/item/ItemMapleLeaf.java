2f55df862e3f76fbb2ce9b96bc4b3d10fb054e1d
Igings <igings@hotmail.co.uk>
2014-08-07 17:49:07 +0100

    Added NBTHelper

--- src/main/java/com/igings/letsmodreboot/item/ItemMapleLeaf.java
+++ src/main/java/com/igings/letsmodreboot/item/ItemMapleLeaf.java
@@ -1,13 +1,21 @@
 package com.igings.letsmodreboot.item;
 
 import com.igings.letsmodreboot.creativetab.CreativeTabLMRB;
+import com.igings.letsmodreboot.utility.NBTHelper;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class ItemMapleLeaf extends ItemLMRB
 {
+    String blockName;
+
     public ItemMapleLeaf()
     {
         super();
         this.setUnlocalizedName("mapleLeaf");
         this.setMaxStackSize(16);
     }
+
 }
